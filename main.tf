f3fd411ffe2bc02bf269bb3bc8e4467332b56b43
+ <+>
2024-10-31 11:09:49 +0300

    feat:add main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Multi Region
+provider "aws" {
+    alias = "us-east-1"
+    region = "us-east-1"
+}
+provider "aws" {
+    alias = "us-west-2"
+    region = "us-west-2"
+}
+
+# Input variable for AMI
+variable "ami_id" {
+    description = "EC2 ami"
+    type = string
+}
+
+# Input variable for the type of instance
+variable "instance_type" {
+    description = "EC2 instance type"
+    type = string
+    default = "t2.micro"
+}
+
+# Create the aws instance resource
+resource "aws_instance" "my_instance" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    tag = {
+        Name = "Instance"
+    }
+}
+
+# Output variable for public ip address
+output "public_ip" {
+    description = "Public ip address of the EC2 instance"
+    value = aws_instance.my_instance.public_ip
+}
