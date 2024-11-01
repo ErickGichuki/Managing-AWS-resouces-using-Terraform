301751cd0a4dffd5cf8de4e93f30c133c747e227
+ <+>
2024-11-01 16:56:19 +0300

    feat:add the variables to just reference

---
+++ RemoteBackend/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+  description = "The ami value for the EC2 instance"
+  type = string
+}
+
+variable "instance_type_value" {
+  description = "The variable that holds the type of the EC2 instance"
+  type = string
+}
