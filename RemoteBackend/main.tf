be6afdabc44184d91db2ed2368028c58e05b7d08
+ <+>
2024-11-01 17:09:17 +0300

    feat:add the aws instance configuration

--- RemoteBackend/main.tf
+++ RemoteBackend/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_instance" "ericksterra" {
-#   ami = var.ami_value
-#   instance_type = var.instance_type_value
-# }
+resource "aws_instance" "ericksterra" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+}
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "erics-terraform-project"
